74b21da7a3389e4aff318b071bbb682a4eb6d29d
flazhy <flazhggmodz.@gmail.com>
2025-05-31 14:00:54 +0800

    Create DataModuleGAG.lua

---
+++ Util/DataModuleGAG.lua
@@ -0,0 +1,36 @@
+local DataModule = {}
+
+DataModule.Seeds = {
+    "Any", "Apple", "Banana", "Bamboo", "Blueberry", "Candy Blossom", "Candy Sunflower", "Carrot", "Cactus",
+    "Chocolate Carrot", "Chocolate Sprinkler", "Coconut", "Corn", "Cranberry", "Cucumber", "Cursed Fruit", "Daffodil",
+    "Dragon Fruit", "Durian", "Easter Egg", "Eggplant", "Grape", "Lemon", "Lotus", "Mango", "Mushroom", "Pepper",
+    "Orange Tulip", "Papaya", "Passionfruit", "Peach", "Pear", "Pineapple", "Pumpkin", "Raspberry", "Red Lollipop",
+    "Soul Fruit", "Strawberry", "Tomato", "Venus Fly Trap", "Watermelon", "Cacao", "Beanstalk"
+}
+
+DataModule.SeedStock = {
+    "Carrot", "Strawberry", "Blueberry", "Orange Tulip", "Tomato", "Corn", "Daffodil", "Watermelon",
+    "Pumpkin", "Apple", "Bamboo", "Coconut", "Cactus", "Dragon Fruit", "Mango", "Grape",
+    "Mushroom", "Pepper", "Cacao", "Beanstalk"
+}
+
+DataModule.Gears = {
+    "Watering Can", "Trowel", "Recall Wrench", "Basic Sprinkler", "Advanced Sprinkler", "Godly Sprinkler",
+    "Lightning Rod", "Master Sprinkler", "Favorite Tool", "Harvest Tool"
+}
+
+DataModule.CratesList = {
+    "Rock Pile", "Small Circle Tile", "BookShelf", "Light On Ground", "Shovel Grave", "Orange Umbrella",
+    "Small Wood Arbour", "Sign Crate", "Common Gnome Crate"
+}
+
+DataModule.BMList = {
+    "Bloodmoon Crate", "Night Egg", "Night Seed Pack", "Blood Banana", "Moon Melon",
+    "Star Caller", "Blood Hedgehog", "Blood Kiwi", "Blood Owl"
+}
+
+DataModule.TwilightList = {
+    "Night Egg", "Night Seed Pack", "Twilight Crate", "Star Caller", "Moon Cat", "Celestiberry", "Moon Mango"
+}
+
+return DataModule
