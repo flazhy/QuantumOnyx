fd54b7fdcfbe8afa2193bd2e55b51a3e5976abeb
flazhy <flazhggmodz.@gmail.com>
2025-05-31 14:48:01 +0800

    Update DataModuleGAG.lua

--- Util/DataModuleGAG.lua
+++ Util/DataModuleGAG.lua
@@ -1,36 +1,34 @@
-local DataModule = {}
+return {
+    Seeds = {
+        "Any", "Apple", "Banana", "Bamboo", "Blueberry", "Candy Blossom", "Candy Sunflower", "Carrot", "Cactus",
+        "Chocolate Carrot", "Chocolate Sprinkler", "Coconut", "Corn", "Cranberry", "Cucumber", "Cursed Fruit", "Daffodil",
+        "Dragon Fruit", "Durian", "Easter Egg", "Eggplant", "Grape", "Lemon", "Lotus", "Mango", "Mushroom", "Pepper",
+        "Orange Tulip", "Papaya", "Passionfruit", "Peach", "Pear", "Pineapple", "Pumpkin", "Raspberry", "Red Lollipop",
+        "Soul Fruit", "Strawberry", "Tomato", "Venus Fly Trap", "Watermelon", "Cacao", "Beanstalk"
+    },
 
-DataModule.Seeds = {
-    "Any", "Apple", "Banana", "Bamboo", "Blueberry", "Candy Blossom", "Candy Sunflower", "Carrot", "Cactus",
-    "Chocolate Carrot", "Chocolate Sprinkler", "Coconut", "Corn", "Cranberry", "Cucumber", "Cursed Fruit", "Daffodil",
-    "Dragon Fruit", "Durian", "Easter Egg", "Eggplant", "Grape", "Lemon", "Lotus", "Mango", "Mushroom", "Pepper",
-    "Orange Tulip", "Papaya", "Passionfruit", "Peach", "Pear", "Pineapple", "Pumpkin", "Raspberry", "Red Lollipop",
-    "Soul Fruit", "Strawberry", "Tomato", "Venus Fly Trap", "Watermelon", "Cacao", "Beanstalk"
+    SeedStock = {
+        "Carrot", "Strawberry", "Blueberry", "Orange Tulip", "Tomato", "Corn", "Daffodil", "Watermelon",
+        "Pumpkin", "Apple", "Bamboo", "Coconut", "Cactus", "Dragon Fruit", "Mango", "Grape",
+        "Mushroom", "Pepper", "Cacao", "Beanstalk"
+    },
+
+    Gears = {
+        "Watering Can", "Trowel", "Recall Wrench", "Basic Sprinkler", "Advanced Sprinkler", "Godly Sprinkler",
+        "Lightning Rod", "Master Sprinkler", "Favorite Tool", "Harvest Tool"
+    },
+
+    CratesList = {
+        "Rock Pile", "Small Circle Tile", "BookShelf", "Light On Ground", "Shovel Grave", "Orange Umbrella",
+        "Small Wood Arbour", "Sign Crate", "Common Gnome Crate"
+    },
+
+    BMList = {
+        "Bloodmoon Crate", "Night Egg", "Night Seed Pack", "Blood Banana", "Moon Melon",
+        "Star Caller", "Blood Hedgehog", "Blood Kiwi", "Blood Owl"
+    },
+
+    TwilightList = {
+        "Night Egg", "Night Seed Pack", "Twilight Crate", "Star Caller", "Moon Cat", "Celestiberry", "Moon Mango"
+    }
 }
-
-DataModule.SeedStock = {
-    "Carrot", "Strawberry", "Blueberry", "Orange Tulip", "Tomato", "Corn", "Daffodil", "Watermelon",
-    "Pumpkin", "Apple", "Bamboo", "Coconut", "Cactus", "Dragon Fruit", "Mango", "Grape",
-    "Mushroom", "Pepper", "Cacao", "Beanstalk"
-}
-
-DataModule.Gears = {
-    "Watering Can", "Trowel", "Recall Wrench", "Basic Sprinkler", "Advanced Sprinkler", "Godly Sprinkler",
-    "Lightning Rod", "Master Sprinkler", "Favorite Tool", "Harvest Tool"
-}
-
-DataModule.CratesList = {
-    "Rock Pile", "Small Circle Tile", "BookShelf", "Light On Ground", "Shovel Grave", "Orange Umbrella",
-    "Small Wood Arbour", "Sign Crate", "Common Gnome Crate"
-}
-
-DataModule.BMList = {
-    "Bloodmoon Crate", "Night Egg", "Night Seed Pack", "Blood Banana", "Moon Melon",
-    "Star Caller", "Blood Hedgehog", "Blood Kiwi", "Blood Owl"
-}
-
-DataModule.TwilightList = {
-    "Night Egg", "Night Seed Pack", "Twilight Crate", "Star Caller", "Moon Cat", "Celestiberry", "Moon Mango"
-}
-
-return DataModule
