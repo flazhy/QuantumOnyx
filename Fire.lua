13ebad7780ac1b46c3e31fcc05c6ed897e166eec
flazhy <flazhggmodz.@gmail.com>
2025-11-19 22:46:54 +0800

    Delete Fire.lua

--- Fire.lua
+++
@@ -1,9 +0,0 @@
-local Fire = {}
-
-local CommF_ = game:GetService("ReplicatedStorage"):WaitForChild("CommF_")
-
-function Fire.RegisterRemote(...)
-    return CommF_:InvokeServer(...)
-end
-
-return Fire
