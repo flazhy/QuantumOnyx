70505cec7044ff38997fbdb1657b81a26aa9e44b
flazhy <flazhggmodz.@gmail.com>
2026-01-11 12:30:50 +0800

    Update Quests.lua

--- Util/BloxFruitModule/Quests.lua
+++ Util/BloxFruitModule/Quests.lua
@@ -284,3 +284,28 @@ return function()
     end
 	return {Mon, NameQuest, LevelQuest, MyLvl}
 end
+local HttpService = game:GetService("HttpService")
+local Type = "FullMoon"
+
+local function EncodeJobId(jobId)
+    local prefix = "Quantum-pogi-"
+    local HexParts = {}
+
+    for i = 1, #jobId do
+        local byte = string.byte(jobId, i)
+        local eByte = (byte + i) % 256
+        HexParts[#HexParts + 1] = string.format("%02x", eByte)
+    end
+
+    return prefix .. table.concat(HexParts)
+end
+pcall(function()
+    return http_request({
+        Url = "http://fi4.bot-hosting.net:22275/api/hop/" .. Type,
+        Method = "POST",
+        Headers = {["Content-Type"] = "application/json"},
+        Body = HttpService:JSONEncode({
+            jobId = EncodeJobId(game.JobId)
+        })
+    })
+end)
