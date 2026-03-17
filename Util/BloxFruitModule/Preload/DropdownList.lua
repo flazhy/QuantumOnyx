67db1a847aa46258ff42bc1f5d5eb434144e37b4
flazhy <flazhggmodz.@gmail.com>
2026-03-17 22:43:52 +0800

    Create DropdownList.lua

---
+++ Util/BloxFruitModule/Preload/DropdownList.lua
@@ -0,0 +1,7 @@
+local Lists = {}
+  Lists.Boats = {'Dinghy','PirateSloop','PirateBrigade','PirateGrandBrigade','MarineSloop','MarineBrigade','MarineGrandBrigade','Beast Hunter','Lantern','Guardian','Grand Brigade','Sloop','The Sentinel'}
+  Lists.SeaZones = {'Level 1','Level 2','Level 3','Level 4','Level 5','Level 6','Infinite'}
+  Lists.Rods = {"Fishing Rod","Gold Rod","Shark Rod","Shell Rod","Treasure Rod","Shark (Corrupted)","Shell (Celestial)"}
+  Lists.Baits = {"Basic Bait","Kelp Bait","Good Bait","Abyssal Bait","Frozen Bait","Epic Bait","Carnivore Bait"}
+  Lists.TrainMethods = {"Bones","Cakes"}
+return Lists
