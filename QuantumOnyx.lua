e34f519224f66337e4153408d8d8904c7dc39d90
flazhy <flazhggmodz.@gmail.com>
2026-03-22 16:13:32 +0800

    Update QuantumOnyx.lua

--- QuantumOnyx.lua
+++ QuantumOnyx.lua
@@ -1,6 +1,6 @@
 local Scripts = {
     [994732206] = "https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/Games/BloxFruits.lua",
-    [9186719164] = "",
+    [9186719164] = "https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/Games/SailorPiece.lua",
 }
 
 local url = Scripts[game.GameId]
