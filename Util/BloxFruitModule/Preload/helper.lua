b8ed0ac88334f0a84f22ea9646e9a5f58b670c3f
flazhy <flazhggmodz.@gmail.com>
2026-03-31 23:01:31 +0800

    Update helper.lua

--- Util/BloxFruitModule/Preload/helper.lua
+++ Util/BloxFruitModule/Preload/helper.lua
@@ -1,11 +1,18 @@
-	local OldNamecall
-	OldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
-		local method = getnamecallmethod()
-		local Name = tostring(self)
-		if not checkcaller() and Name == "PlayerGui" then
-			if method == "Destroy" or method == "Remove" or method == "ClearAllChildren" then
-				return
-			end
-		end
-		return OldNamecall(self, ...)
-	end))
+local Players = game:GetService("Players")
+local plr = Players.LocalPlayer
+local PlayerGui = plr:WaitForChild("PlayerGui")
+
+local OldNamecall
+OldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+    local method = getnamecallmethod()
+    if checkcaller() then
+        return OldNamecall(self, ...)
+    end
+    if typeof(self) == "Instance" and self == PlayerGui then
+        if method == "Destroy" or method == "Remove" or method == "ClearAllChildren" then
+            return nil
+        end
+    end
+
+    return OldNamecall(self, ...)
+end))
