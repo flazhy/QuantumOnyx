7d177943b06cc0244bab1210cce33c8b0405df25
flazhy <flazhggmodz.@gmail.com>
2026-04-02 11:57:18 +0800

    Update helper.lua

--- Util/BloxFruitModule/Preload/helper.lua
+++ Util/BloxFruitModule/Preload/helper.lua
@@ -1,18 +1,28 @@
-local Players = game:GetService("Players")
-local plr = Players.LocalPlayer
-local PlayerGui = plr:WaitForChild("PlayerGui")
+local Guard = (function()
+    local Players = game:GetService("Players")
+    local plr = Players.LocalPlayer
+    local PlayerGui = plr:WaitForChild("PlayerGui")
+    local BLOCKED_METHODS = {
+        Destroy = true,
+        Remove = true,
+        ClearAllChildren = true,
+    }
+    local OldNamecall
+    OldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+        local method = getnamecallmethod()
 
-local OldNamecall
-OldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
-    local method = getnamecallmethod()
-    if checkcaller() then
-        return OldNamecall(self, ...)
-    end
-    if typeof(self) == "Instance" and self == PlayerGui then
-        if method == "Destroy" or method == "Remove" or method == "ClearAllChildren" then
+        if checkcaller() then
+            return OldNamecall(self, ...)
+        end
+        if typeof(self) == "Instance" and self == PlayerGui and BLOCKED_METHODS[method] then
+            task.defer(function()
+                warn(("[Guard] Blocked '%s' on PlayerGui"):format(method))
+            end)
             return nil
         end
-    end
-
-    return OldNamecall(self, ...)
-end))
+        return OldNamecall(self, ...)
+    end))
+    task.defer(function()
+        print("[Guard] PlayerGui protection active")
+    end)
+end)()
