5432234da807a91520d5184558f44b8d2ef50513
flazhy <flazhggmodz.@gmail.com>
2026-05-05 19:57:18 +0800

    Update QuantumOnyx.lua

--- QuantumOnyx.lua
+++ QuantumOnyx.lua
@@ -1,7 +1,9 @@
+local Directory = "https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/Games"
 local Scripts = {
-    [994732206] = "https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/Games/BloxFruits.lua",
-    [9186719164] = "https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/Games/SailorPiece.lua",
-    [8191429227] = "https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/Games/CutTrees.lua",
+    [994732206] = Directory.."/BloxFruits.lua",
+    [9186719164] = Directory.."/SailorPiece.lua",
+    [8191429227] = Directory.."/CutTrees.lua",
+    [9792947201] = Directory.."/SlimeRNG.lua",
 }
 
 local url = Scripts[game.GameId]
